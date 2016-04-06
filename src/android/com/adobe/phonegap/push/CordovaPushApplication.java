330ae8bd4f67c5bdf91902c27ef309ad402ab528
Edwin <edwin@myorpheo.com>
2016-04-06 14:38:13 +0200

    android notifications stable

--- src/android/com/adobe/phonegap/push/CordovaPushApplication.java
+++ src/android/com/adobe/phonegap/push/CordovaPushApplication.java
@@ -4,7 +4,7 @@ package com.adobe.phonegap.push;
  * Created by MyOrpheoEdwin on 01/04/16.
  */
 import android.app.Application;
-
+import android.util.Log;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 
@@ -14,16 +14,11 @@ public class CordovaPushApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-
-
         // Parse init
-       
-
-        Log.d("Parse Installation", "Initializing with parse_app_id: " + appId + " and parse_client_key:" + clientKey);
-
         String appId = getStringByKey(this, "parse_app_id");
         String clientKey = getStringByKey(this, "parse_client_key");
+
+        Log.d("Parse Installation", "Initializing with parse_app_id: " + appId + " and parse_client_key:" + clientKey);
 
         Parse.initialize(this, appId, clientKey);
         ParseInstallation.getCurrentInstallation().saveInBackground();
