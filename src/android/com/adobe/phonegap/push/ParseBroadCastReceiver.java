14f582367d86da7d4c2c4b3903c64a5d23a17e09
Edwin <edwin@myorpheo.com>
2016-04-06 10:59:07 +0200

    disable notification when unregister

---
+++ src/android/com/adobe/phonegap/push/ParseBroadCastReceiver.java
@@ -0,0 +1,17 @@
+package com.adobe.phonegap.push;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+/**
+ * Created by MyOrpheoEdwin on 06/04/16.
+ */
+public class ParseBroadCastReceiver extends ParsePushBroadcastReceiver{
+
+    @Override
+    protected void onPushReceive(Context context, Intent intent) {
+        //
+    }
+}
